dbb09d1b1f3c40a02e3ef8fb4ae8288d70fbe3ba
pazIago <iagopaz@hotmail.com.br>
2022-06-20 15:40:34 -0300

    button navigation added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-import Slide from "./slide.js";
+import {Slide, SlideNav} from "./slide.js";
 
-const slide = new Slide('.slide','.wrapper');
+const slide = new SlideNav('.slide','.wrapper');
 slide.init()
-slide.changeSlide(1)
-slide.activeNextSlide()
+
+slide.addArrow('.prev','.next')
+console.log(slide)
